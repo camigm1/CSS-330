41c09512bc08a954e98c5b1cdb3d26dd2857df9d
Camila Gallegos <camigm1@gmail.com>
2022-05-24 14:28:59 -0600

    updates

--- week 6/main.js
+++ week 6/main.js
@@ -1,13 +1,25 @@
+let todoItem = document.querySelector("#input-todo");
 const button = document.querySelector("#btn");
 const ul = document.querySelector("ul");
+const btnAll = document.querySelector("#btn-all");
+const btnActive = document.querySelector("#btn-active");
+const btnComplete = document.querySelector("#btn-complete");
 
 button.addEventListener("click", addToDo);
 
 function addToDo() {
-  let todoItem = document.querySelector("#input-todo").value;
   let item = document.createElement("li");
-  item.textContent = todoItem;
+  item.textContent = todoItem.value;
   console.log(todoItem.value);
   item.style.color = "black";
   ul.append(item);
+  clearItem();
 }
+
+function clearItem() {
+  todoItem.value = "";
+}
+
+btnComplete.addEventListener("click", completeItem);
+
+function completeItem() {}
