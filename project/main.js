f3745147c0625110edea807c9ac17c9747f0e9ad
Camila Gallegos <camigm1@gmail.com>
2022-07-06 21:41:13 -0600

    update

---
+++ project/main.js
@@ -0,0 +1,4 @@
+// Using an external Web api to retrieve data
+// Doing some form of data input and validation, like a search, or form information input.
+// Using localstorage to persist data across refreshes
+// Using your css and html skills learned to make the application look good.
